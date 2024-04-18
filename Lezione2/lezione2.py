6bec92cf31aa1035bffb566acd9a16f3cc37b72f
TigerBlaze24 <cuanbellina@gmail.com>
2024-04-18 18:51:23 +0200

    lezione2.py

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -2,3 +2,57 @@
 #18/04/2024
 
 print("hello world")
+#2.3)usa una variabile per rappresentare un nome di una persona, stampa il messaggio della persona
+name :str = "Mary"
+print("ciao ti và di mparare un pò di python insieme?")
+#oppure ,così in cui la variabile contiene il messaggio
+message: str = f"ciao{name}, ti và di imparare un pò di python insieme?"
+
+#2.4)
+#questa variabile contiene il nome di una persona
+name: str ="luca"
+
+#questa variabile contiene il nome in minuscolo
+name_lower: str = name.lower()
+
+#questa variabile contiene il nome in maiuscolo
+name_upper: str = name.upper()
+
+print( f" {name}, {name.lower} ,{name.upper}")
+
+
+
+#2.5)
+autore :str='Giuliocesare'
+frase = "quoue tu bruts fili mi"
+print(frase)
+
+#2.6)
+famousperson:str="famousperson"
+message = "piange il telefono"
+print(message)
+
+#2.8)
+filename:str ="prova.txt"
+print(filename.removesuffix(".txt"))
+
+#3.2)
+name =["luca","elena","gino" , "giuseppe"]
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+
+#3.3)
+print("ciao best " , nomi[0] , "come stai?")
+print("ciao tata", nomi [1] ,"stai andado a casa?")
+print("ciao bello" , nomi [2] , "vieni al cinema?")
+print("ciao splendore" , nomi [3] , "vieni dal paninaro stasera?")
+print("ciao bellssimo" , nomi [4] , "con me dal parrucchiere?")
+#da finire e poi scriverli
+
+
+
+
+
